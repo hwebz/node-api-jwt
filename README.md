74e3df788f6d54748881774e9e4480d309f25c73
Ha Manh Do <ha.do@smartbox.com>
2019-09-11 11:09:45 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Nodejs Express ResfulAPI with JWT
+
+A ResfulAPI with JWT using Nodejs and Express
+
+# Prerequisites
+- Node
+- NPM
+- Git
+- MongoDB
+- Express
+
+## Commands
+For install dependencie
+> npm install
+
+For start dev server on local (Nodejs)
+> npm start
---
+++ routes/posts.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const verify = require('./verifyToken')
+
+router.get('/', verify, (req, res) => {
+    res.json({
+        posts: {
+            title: "My First Post",
+            description: "Random data you shouldn't access"
+        },
+        user: req.user
+    });
+})
+
+module.exports = router;
